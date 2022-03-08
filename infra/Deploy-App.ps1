65c396fbc1eb8b4a1f9f6bade79ad7f1bc10f1e3
Justin Yoo <justin.yoojh@gmail.com>
2022-03-09 08:58:16 +0900

    Update GHA workflows

--- infra/Deploy-App.ps1
+++ infra/Deploy-App.ps1
@@ -1,4 +1,3 @@
 # Deploy app
 
-echo '{ "jobName": "Deploy Static Web App", "appLocation": "app", "apiLocation": "api", "outputLocation": "wwwroot" }' `
-    | gh workflow run "App Deploy Dispatch" --repo devkimchi/ASWA-AutoPilot-Sample --json
+gh workflow run "App Deploy Dispatch" --repo devkimchi/ASWA-AutoPilot-Sample
